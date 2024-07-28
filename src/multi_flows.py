d05d0c13e840aab4f1129223c6b97f36f6b4340b
Juan Esteban Grimaldos León <123501155+juanes-grimaldos@users.noreply.github.com>
2024-07-28 19:13:18 +0000

    refactor multi flow script

--- src/multi_flows.py
+++ src/multi_flows.py
@@ -1,22 +1,25 @@
 from prefect import serve
 from workflow import workflow
-from data_monitoring import data_monitoring
-
+from data_monitoring import batch_monitoring_backfill
+import os
 
 if __name__ == "__main__":
-    task1_deploy = workflow.to_deployment(
+    workflow_deploy = workflow.to_deployment(
         name='workflow',
         cron='*/15 * * * *',
         tags=['mlops', 'tracking'],
         description='keep track on the model performance',
-        version='0.1.0')
-    task2_deploy = data_monitoring.to_deployment(
-        name='data_monitoring',
-        cron='*/10 * * * *',
-        tags=['evently', 'grafana'],
-        description='monitor the data',
-        version='0.1.0')
+        version='0.1.0'
+    )
+    monitoring_deploy = batch_monitoring_backfill.to_deployment(
+    name='data_monitoring',
+    cron='*/10 * * * *',
+    tags=['evently', 'grafana'],
+    description='monitor the data',
+    version='0.1.0'
+    )
+
     serve(
-        task1_deploy,
-        task2_deploy,
+        workflow_deploy,
+        monitoring_deploy
     )
